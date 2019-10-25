53211cf397f7223f5acb78526be3175dbadbbf25
Michael Lese <michael.lese@commissionsinc.com>
2019-10-25 14:28:46 -0400

    Fancy Record Button

--- Camera2VideoKotlin/Application/src/main/java/com/example/android/camera2video/CaptureButtonTest.kt
+++ Camera2VideoKotlin/Application/src/main/java/com/example/android/camera2video/CaptureButtonTest.kt
@@ -2,11 +2,13 @@ package com.example.android.camera2video
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_capture_button_test.*
 
 class CaptureButtonTest : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_capture_button_test)
+        record_button.limit = 90
     }
 }
--- Camera2VideoKotlin/Application/src/main/java/com/example/android/camera2video/RecordButton.kt
+++ Camera2VideoKotlin/Application/src/main/java/com/example/android/camera2video/RecordButton.kt
@@ -1,15 +1,50 @@
 package com.example.android.camera2video
 
 import android.content.Context
+import android.os.Handler
 import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
+import android.view.MotionEvent
+import kotlinx.android.synthetic.main.record_button.view.*
 
 class RecordButton: ConstraintLayout {
     constructor(context: Context): this(context, null)
     constructor(context: Context, attrs: AttributeSet?): this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int): super(context, attrs, defStyleAttr)
 
+    var limit = 20
+    val maxProgress = 10000
+    val interval = maxProgress / ((limit * 1000) / 30)
+    val progressUpdateHandler = Handler()
+    var progressRunning = false
+
     init {
         inflate(context, R.layout.record_button, this)
+        progressBar.setOnTouchListener { v, event ->
+            when(event?.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    progressRunning = true
+                    handler.post(progressRunnable)
+                }
+                MotionEvent.ACTION_UP -> {
+                    progressRunning = false
+                    progressBar.progress = -1
+                }
+            }
+            super.onTouchEvent(event)
+        }
     }
+
+    val progressRunnable: Runnable = object: Runnable {
+        override fun run() {
+            progressBar.progress = progressBar.progress + interval
+            if (progressBar.progress > maxProgress) progressRunning = false
+            if (progressRunning) {
+                progressUpdateHandler.postDelayed(this, 30)
+            } else {
+                progressBar.progress = 0
+            }
+        }
+    }
+
 }
